0742660ec6c4838c7284e440b3e4f08a61407341
CryptoThibault <thibault.chalaoux@gmail.com>
2021-04-07 15:47:14 +0200

    bonus

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,45 @@
+import { useState } from 'react'
 import Product from './Product'
 
 function ShoppingList({ shopping, removeFromShoppingList }) {
+  const [filter, setFilter] = useState("")
+  const filteredList = shopping.filter((el) =>
+    el.trim().toLowerCase().startsWith(filter.trim().toLowerCase())
+  )
   return (
     <>
       <h2 className="mb-3 h4">Produits à acheter ({shopping.length}):</h2>
-      <ul className="list-group mb-3 shadow">
-        {shopping.map((product) => {
+      <div className="input-group mb-3">
+        <span role="img" aria-label="search" className="input-group-text">
+          🔎
+        </span>
+        <input
+          type="search"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          placeholder="Rechercher dans votre liste des courses ..."
+          aria-label="Chercher"
+          className="form-control"
+        />
+      </div>
+      {filter && (
+        <p className="d-flex justify-content-between">
+          <span>
+            Produits qui commencent par <i>{filter}</i>
+          </span>
+          <button
+            className="btn btn-light btn-sm"
+            onClick={() => setFilter("")}
+          >
+            <span role="img" aria-hidden>
+              🔄
+            </span>{" "}
+            Réinitialiser
+          </button>
+        </p>
+      )}
+      <ol className="list-group mb-3 shadow">
+        {filteredList.map((product) => {
           return (
             <li className="list-group-item" key={product}>
               <Product
@@ -15,7 +49,7 @@ function ShoppingList({ shopping, removeFromShoppingList }) {
             </li>
           )
         })}
-      </ul>
+      </ol>
     </>
   )
 }
